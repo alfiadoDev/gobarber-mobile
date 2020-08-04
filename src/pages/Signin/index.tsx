f16be69b96786139f2d7611cffb26cc3f93c203c
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-04 14:01:01 +0200

    configurando navegacao

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Signin: React.FC = () => (
+  <Container>
+    <Text>Signin</Text>
+  </Container>
+);
+
+export default Signin;
