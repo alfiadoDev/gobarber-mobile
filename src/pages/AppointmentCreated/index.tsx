dda6e5fb3e0a838d165246dc7d8af5576df01efe
Alfiado Constantino <alfiado02@gmail.com>
2020-11-12 14:06:00 +0200

    criando paginas e rotas

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AppointmentCreated: React.FC = () => {
+  return (
+    <View>
+      <Text>Appointment Created</Text>
+    </View>
+  );
+};
+
+export default AppointmentCreated;
