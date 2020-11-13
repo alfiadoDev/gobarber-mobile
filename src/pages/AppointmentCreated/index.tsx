ce5f8e825323fcf83190696a0e56903f56d7119c
Alfiado Constantino <alfiado02@gmail.com>
2020-11-13 17:47:50 +0200

    criando a estrutura da tela de sucesso

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OKButton,
+  OKButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
-    <View>
-      <Text>Appointment Created</Text>
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluido</Title>
+      <Description>Sexta, dia 13 de marco de 2020 as 17:00</Description>
+
+      <OKButton onPress={handleOkPressed}>
+        <OKButtonText>Ok</OKButtonText>
+      </OKButton>
+    </Container>
   );
 };
 
